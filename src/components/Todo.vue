7ba6864a5ce00bed2c9aaf8ff0d60c611ceb6ba8
Christopher Deuda <christopherdeuda@gmail.com>
2018-11-13 16:51:02 +0800

    Add seperate componet for Todo

---
+++ src/components/Todo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class='ui centered card'   v-bind:key="todo">
+        <div class="content">
+            <div class="header">
+                {{ todo.title}}
+            </div>
+            <div class="meta">
+                {{ todo.project}}
+
+            </div>
+            <div class="extra content">
+                <span class='right floated edit icon'>
+                    <i class='edit icon'></i>
+                </span>
+            </div>
+        </div>
+        <div class="ui bottom attached green basic button">
+            Completed
+        </div>
+        <div class="ui bottom attached red basic button" >
+            Complete
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['todo'],
+}
+</script>
