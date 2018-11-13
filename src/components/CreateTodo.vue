f1cbe60cc4d1cce273e76c8248395fd76a2e91f1
Christopher Deuda <christopherdeuda@gmail.com>
2018-11-13 17:32:29 +0800

    Add createtodo component

---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="ui basic content center aligned segment">
+        <button class="ui basic button icon"> 
+            <i class="plus icon"></i>
+        </button>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
