f4e18088b775979e404d600bc5e691bb19ddd8d4
Christopher Deuda <christopherdeuda@gmail.com>
2018-11-13 17:50:36 +0800

    Add createTodo Functionality

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,21 @@
   <div id="app">
     <!-- <router-view/> -->
     <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:add-todo="addTodo"></create-todo>
+
   </div>
 </template>
 
 <script>
 
 import TodoList from "@/components/TodoList";
+import CreateTodo from "@/components/CreateTodo";
 
 export default {
   name: 'App',
   components: {
-    TodoList
+    TodoList,
+    CreateTodo,
   },
 
   data () {
--- src/components/CreateTodo.vue
+++ src/components/CreateTodo.vue
@@ -3,12 +3,51 @@
         <button class="ui basic button icon"> 
             <i class="plus icon"></i>
         </button>
-
+        <div class="ui centered card" v-on:click="openForm" v-show="!isCreating">
+            <div class="content">
+                <div class="ui form">
+                    <div class="field">
+                        <label> Title</label>
+                        <input v-model="titleText" type="text" ref="title" defaultValue="">
+                    </div>
+                    <div class="field">
+                        <label> Project</label>
+                        <input type="text" ref="project" defaultValue="">
+                    </div>
+                     <div class='ui two button attached buttons'>
+                        <button class='ui basic blue button' v-on:click="sendForm()">
+                            Create
+                        </button>
+                        <button class='ui basic red button' v-on:click="closeForm">
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return {
+            titleText: '',
+            projectText: '',
+            isCreating: false,
+        };
+    },
+    methods: {
+        openForm(){
+            this.isCreating = true;
+        },
+        closeForm(){
+            this.isCreating = false;
+        },
+        sendForm(){
+
+        }
+    }
     
 }
 </script>
