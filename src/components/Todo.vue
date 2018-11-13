9b83cd11caf43ad12e3c13e0a7c76a7f299c3192
Christopher Deuda <christopherdeuda@gmail.com>
2018-11-13 16:58:02 +0800

    Add form editing

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -14,6 +14,22 @@
                 </span>
             </div>
         </div>
+         <!-- Form is visible when we are in editing mode -->
+        <div class="content" v-show="isEditing">
+            <div class="ui form">
+                <div class="field">
+                    <label> Title </label>
+                    <input type="text" v-model="todo.title">
+                </div>
+                <div class="field">
+                    <label> Project </label>
+                    <input type="text" v-model="todo.project">
+                </div>
+                <div class="ui two button attached buttons">
+                    <button class="ui basic blue botton" v-on:click="hideForm"> Close X</button>
+                </div>
+            </div>
+        </div>
         <div class="ui bottom attached green basic button">
             Completed
         </div>
